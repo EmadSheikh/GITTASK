0067b67dde47a592a9aea979abc70bcd0eef6c7c
EmadSheikh <emad_3306@hotmail.com>
2016-11-05 08:06:49 +0500

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ //Emad's Code
+ int a,b,c;
+ 
+ cout << "Enter two numbers: ";
+ cin >> a;
+ cin >> b;
+ 
+ c = a + b;
+ cout << "Sum is: " << c << endl;
+ 
+ return 0;
+}
